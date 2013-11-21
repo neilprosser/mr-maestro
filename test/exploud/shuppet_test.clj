0de3b1b4e69b2ca196f021bb5e5338c42f9d00da
micbell <michael.1.bell@nokia.com>
2013-11-21 14:53:12 +0000

    Changes to adjust to simpler call to Shuppet API.

--- test/exploud/shuppet_test.clj
+++ test/exploud/shuppet_test.clj
@@ -14,24 +14,11 @@
 (fact "that upserting an application creates a new one if shuppet responds OK."
       (upsert-application ..name..) => dummy-app
       (provided
-       (get-application ..name..) => nil
        (http/simple-post anything) =>  {:status 201
                                         :body (json/generate-string dummy-app)}))
 
 (fact "that upserting an application throws an error if shuppet returns bad status."
          (upsert-application ..name..) => (throws ExceptionInfo)
          (provided
-          (get-application ..name..) => nil
           (http/simple-post anything) =>  {:status 500
                                            :body ..body..}))
-
-(fact "that upserting an application creates a new one if it didn't exist."
-      (upsert-application ..name..) => ..application..
-      (provided
-       (get-application ..name..) => nil
-       (create-application ..name..) => ..application..))
-
-(fact "that upserting an application fetches the existing one if it exists."
-      (upsert-application ..name..) => ..application..
-      (provided
-       (get-application ..name..) => ..application..))
