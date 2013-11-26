79cad7e8f7502c7bfa01ae7e5e5d6e83b6fe6ec1
micbell <michael.1.bell@nokia.com>
2013-11-26 17:22:00 +0000

    New version of nokia/ring-utils to handle ExceptionInfo instances.

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
                  [environ "0.4.0"]
                  [metrics-clojure "1.0.1"]
                  [metrics-clojure-ring "1.0.1"]
-                 [nokia/ring-utils "1.2.0"]
+                 [nokia/ring-utils "1.2.1"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.3"]
                  [org.clojure/data.xml "0.0.7"]
